5619e2a07b900f7a7ff18f0292411aca84480959
Mouheb SLITI <m.sliti@sofrecom-tunisie.com>
2023-04-18 14:02:20 +0100

    Error handling and data validation

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
     port: 12345,
-    mongoURI: 'mongodb+srv://mouhebsliti:mouheb3006@sofreverse.qkzravb.mongodb.net/test',
+    mongoURI: 'mongodb+srv://mouhebsliti:mouheb3006@backendpfe.7jx5gaz.mongodb.net/test',
 };
