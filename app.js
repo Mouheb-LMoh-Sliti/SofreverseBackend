f5f57c7a7a9189bda76f3222abb23378747ad5e0
Mouheb SLITI <m.sliti@sofrecom-tunisie.com>
2023-04-18 14:36:29 +0100

    Error handling and data validation

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+
+import express from 'express';
+import mongoose from 'mongoose';
+import Account from './model/Account';
+import authentication from './routes';
+
 const express = require('express');
 const keys = require('./config/keys.js');
 const app = express();
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json());
 
 // Setting up DB
 const mongoose = require('mongoose');
@@ -16,9 +23,10 @@ require('./model/Account');
 // Setup the routes
 require('./routes/authenticationRoutes')(app);
 
-app.get('/test/', function(req, res){
-    res.send("Hello from the root application URL");
-});
+mongoose.set("debug", true);
+mongoose.Promise=global.Promise;
+
+
 
 app.listen(keys.port, () => {
     console.log("Listening on " + keys.port);
--- model/Account.js
+++ model/Account.js
@@ -1,22 +1,54 @@
+import mongoose from 'mongoose';
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const accountSchema = new Schema({
-   
-    username: 
-        { 
-        type: String,
-        unique: [true, "username already taken"]
-        },
-    email:String,
-    password: String,
-    salt: String,
-    experience:Number,
-    level:Number,
-    avatarPreset: String,
-    lastAuthentication: Date,
+const accountSchema = new Schema(
+    {
+        username: 
+           { 
+                type: String,
+                unique: [true, "username already taken"],
+                required : true
+            },
+
+        email :
+            { 
+                type: String,
+                unique: [true, "mail already taken"],
+                required : true
+             },
+
+        password: 
+             { 
+                type: String,
+                required : true
+             },
+
+        salt: 
+            { 
+                type: String,
+                required : true
+            },
+            
+        experience : 
+            { 
+                type: Number,
+            },
+        
+        level: 
+            { 
+                type: Number,
+             },
+
+        avatarPreset: 
+            {
+               type: String,
+               required: true
+            },
+    
+            lastAuthentication: Date,
    
 });
 
-
+export default model ("accounts",accountSchema);
 mongoose.model('accounts', accountSchema);
