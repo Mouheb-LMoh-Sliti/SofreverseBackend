64acc6b62264ddc34e12b79c79723637a57fe48f
Mouheb SLITI <m.sliti@sofrecom-tunisie.com>
2023-04-19 10:52:31 +0100

    Adding controller and fixing script

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-    port: 12345,
-    mongoURI: 'mongodb+srv://mouhebsliti:mouheb3006@backendpfe.7jx5gaz.mongodb.net/test',
+    port: 9090,
+    mongoURI: 'mongodb+srv://mouhebsliti:mouheb3006@sofreverse.qkzravb.mongodb.net/test',
 };
