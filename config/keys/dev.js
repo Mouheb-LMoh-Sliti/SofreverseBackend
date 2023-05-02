4cab4a0fe72e877a10a2762bdc717cfe93d0f1f9
Mouheb SLITI <m.sliti@sofrecom-tunisie.com>
2023-05-02 10:11:12 +0100

    Friends service : add and get list

--- config/keys/dev.js
+++ config/keys/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: 9090,
-    mongoURI: 'mongodb+srv://mouhebsliti:mouheb3006@backendpfe.7jx5gaz.mongodb.net/test',
+    mongoURI: 'mongodb+srv://mouhebsliti:mouheb3006@sofreverse.qkzravb.mongodb.net/test',
   };
   
