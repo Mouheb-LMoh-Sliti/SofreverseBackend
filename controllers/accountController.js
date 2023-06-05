800ccfe51a26ca85879d98696ed6e2b4cf0070cf
Mouheb SLITI <m.sliti@sofrecom-tunisie.com>
2023-06-05 09:36:18 +0100

    UpdateXp Service

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -151,7 +151,7 @@ const updateXp = async (req, res) => {
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
-    user.experience = xp;
+    user.experience += xp; // Add the value of xp to the existing experience
     await user.save();
     return res.status(200).json({ message: "Experience updated successfully" });
   } catch (err) {
